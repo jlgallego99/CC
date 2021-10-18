c52090b73602e8bf55badd7a8dc93564d34e3491
Dunspa <jlgallego99@correo.ugr.es>
2021-10-18 17:12:43 +0200

    Interfaz de operaciones para obtener la lista de canciones, momento y añadir cancion #10

--- internal/obra/obra.go
+++ internal/obra/obra.go
@@ -4,6 +4,12 @@ import (
 	"github.com/jlgallego99/OSTfind/internal/cancion"
 )
 
+type Obra interface {
+	Canciones() []cancion.Cancion
+	Momento() string
+	nuevaCancion(c cancion.Cancion)
+}
+
 type Videojuego struct {
 	titulo    string
 	canciones []cancion.Cancion
@@ -17,4 +23,43 @@ type Pelicula struct {
 type Serie struct {
 	titulo    string
 	canciones []cancion.Cancion
+}
+
+func newObra(titulo string, canciones []cancion.Cancion) {
+}
+
+func (v *Videojuego) Canciones() []cancion.Cancion {
+	return nil
+}
+
+func (p *Pelicula) Canciones() []cancion.Cancion {
+	return nil
+}
+
+func (s *Serie) Canciones() []cancion.Cancion {
+	return nil
+}
+
+func (v *Videojuego) Momento() string {
+	return ""
+}
+
+func (p *Pelicula) Momento() string {
+	return ""
+}
+
+func (s *Serie) Momento() string {
+	return ""
+}
+
+func (v *Videojuego) nuevaCancion() {
+
+}
+
+func (p *Pelicula) nuevaCancion() {
+
+}
+
+func (s *Serie) nuevaCancion() {
+
 }
