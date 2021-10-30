31b4056a6d8ef259cd8692d1d08974e34a6ff4fd
Dunspa <jlgallego99@correo.ugr.es>
2021-10-30 16:25:57 +0200

    Distinguir usuarios mediante structs distintos #18

--- internal/usuario/usuario.go
+++ internal/usuario/usuario.go
@@ -2,6 +2,15 @@ package usuario
 
 import "github.com/jlgallego99/OSTfind/internal/cancion"
 
+type Colaborador struct {
+	Nombre             string
+	CancionesFavoritas []cancion.Cancion
+}
+
+type Buscador struct {
+	Nombre string
+}
+
 type Usuario interface {
 	Like(c cancion.Cancion) error
 	Dislike(c cancion.Cancion) error
