7a45493595219a23d6b371d3ec546b7f680f44b9
Dunspa <jlgallego99@correo.ugr.es>
2021-10-30 20:29:00 +0200

    Testear que al añadir la cancion se comprueba si existe y si da error #23

--- internal/obra/obra_test.go
+++ internal/obra/obra_test.go
@@ -13,6 +13,7 @@ var _ = Describe("Obra", func() {
 	var pelicula *obra.Pelicula
 	var videojuego *obra.Videojuego
 	var cancionCorrecta cancion.Cancion_info
+	var err_s, err_p, err_v error
 
 	BeforeEach(func() {
 		cancionCorrecta = cancion.Cancion_info{
@@ -53,6 +54,52 @@ var _ = Describe("Obra", func() {
 			It("Debe tener todos los campos iguales", func() {
 				Expect(videojuego.Titulo).To(Equal("VideojuegoPrueba"))
 				Expect(videojuego.OST).To(Equal([]cancion.Cancion_info{cancionCorrecta, {}}))
+			})
+		})
+	})
+
+	Describe("Añadir una canción a una obra", func() {
+		BeforeEach(func() {
+			serie.OST = []cancion.Cancion_info{}
+			pelicula.OST = []cancion.Cancion_info{}
+			videojuego.OST = []cancion.Cancion_info{}
+
+			err_s = serie.NuevaCancion(cancionCorrecta)
+			err_p = pelicula.NuevaCancion(cancionCorrecta)
+			err_v = videojuego.NuevaCancion(cancionCorrecta)
+		})
+
+		Context("Se añade una canción nueva", func() {
+			It("Debe existir la canción en la OST", func() {
+				Expect(serie.OST[0]).To(Equal(cancionCorrecta))
+				Expect(pelicula.OST[0]).To(Equal(cancionCorrecta))
+				Expect(videojuego.OST[0]).To(Equal(cancionCorrecta))
+			})
+
+			It("No debe dar error", func() {
+				Expect(err_s).NotTo(HaveOccurred())
+				Expect(err_p).NotTo(HaveOccurred())
+				Expect(err_v).NotTo(HaveOccurred())
+			})
+		})
+
+		Context("Se añade una canción que ya existe en la OST", func() {
+			BeforeEach(func() {
+				err_s = serie.NuevaCancion(cancionCorrecta)
+				err_p = pelicula.NuevaCancion(cancionCorrecta)
+				err_v = videojuego.NuevaCancion(cancionCorrecta)
+			})
+
+			It("No debe haber más de una canción en la OST", func() {
+				Expect(len(serie.OST)).To(Equal(1))
+				Expect(len(pelicula.OST)).To(Equal(1))
+				Expect(len(videojuego.OST)).To(Equal(1))
+			})
+
+			It("Debe dar error", func() {
+				Expect(err_s).To(HaveOccurred())
+				Expect(err_p).To(HaveOccurred())
+				Expect(err_v).To(HaveOccurred())
 			})
 		})
 	})
