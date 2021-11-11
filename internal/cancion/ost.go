e76bcf15840cdc89d40f3920a088c5e31b57152e
Dunspa <jlgallego99@correo.ugr.es>
2021-11-11 17:11:37 +0100

    Para actualizar la ost se crea una funcion que las añade individualmente comprobando si ya existen #41

--- internal/cancion/ost.go
+++ internal/cancion/ost.go
@@ -1,6 +1,10 @@
 package cancion
 
 import (
+	"errors"
+	"fmt"
+	"reflect"
+
 	"github.com/jlgallego99/OSTfind/internal/obra"
 )
 
@@ -13,4 +17,32 @@ type OST interface {
 	ActualizarOST(ost []Cancion_info) error
 	NuevaCancion(c Cancion_info) error
 	Cancion(titulo string) error
+}
+
+func (b *BandaSonora) ActualizarOST(ost []Cancion_info) error {
+	b.canciones = make([]*Cancion_info, 0)
+
+	for _, v := range ost {
+		err := b.NuevaCancion(v)
+
+		if err != nil {
+			return fmt.Errorf("no se ha podido añadir la canción: %s", err)
+		}
+	}
+
+	return nil
+}
+
+func (b *BandaSonora) NuevaCancion(c Cancion_info) error {
+	if existe, _ := c.ExisteEn(b.canciones); existe {
+		return errors.New("la canción ya existe en la OST")
+	}
+
+	if reflect.DeepEqual(Cancion_info{}, c) {
+		return errors.New("la canción está vacía")
+	}
+
+	b.canciones = append(b.canciones, &c)
+
+	return nil
 }
