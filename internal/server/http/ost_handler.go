451bf05b709b4635ee8693f23e1a197b526f111f
Dunspa <jlgallego99@correo.ugr.es>
2021-12-05 14:46:22 +0100

    El recurso principal es la ost #67
    Para añadir y recuperar ost y canciones de esa ost

--- internal/server/http/ost_handler.go
+++ internal/server/http/ost_handler.go
@@ -5,3 +5,7 @@ import "github.com/gin-gonic/gin"
 func NewOST(c *gin.Context) {
 
 }
+
+func GetOST(c *gin.Context) {
+
+}
--- internal/server/http/routes.go
+++ internal/server/http/routes.go
@@ -8,6 +8,7 @@ func startRoutes() *gin.Engine {
 	router := gin.Default()
 
 	router.POST("/osts/:obra/:ost", NewOST)
+	router.GET("/osts/:obra/:ost", GetOST)
 
 	return router
 }
