37a9fe860879555b20d39ec3587df30ae29a075e
Dunspa <jlgallego99@correo.ugr.es>
2021-12-05 14:44:34 +0100

    Crear ost con POST #67
    Indicando el tipo de obra y el nombre de la ost en la uri

--- internal/server/http/ost_handler.go
+++ internal/server/http/ost_handler.go
@@ -1,1 +1,7 @@
 package http
+
+import "github.com/gin-gonic/gin"
+
+func NewOST(c *gin.Context) {
+
+}
--- internal/server/http/routes.go
+++ internal/server/http/routes.go
@@ -7,5 +7,7 @@ import (
 func startRoutes() *gin.Engine {
 	router := gin.Default()
 
+	router.POST("/osts/:obra/:ost", NewOST)
+
 	return router
 }
