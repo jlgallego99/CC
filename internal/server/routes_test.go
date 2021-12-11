9835e252062e6bd5a984e28cde74c8e1e30b4a55
Dunspa <jlgallego99@correo.ugr.es>
2021-12-11 18:39:33 +0100

    Extraer json usando structs #67
    Teniendo asi una estructura de respuesta http concreta y facil de tratar

--- internal/server/routes_test.go
+++ internal/server/routes_test.go
@@ -7,18 +7,31 @@ import (
 	"net/http/httptest"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jlgallego99/OSTfind/internal/cancion"
 	"github.com/jlgallego99/OSTfind/internal/server"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
+
+type Respuesta struct {
+	Message string        `json:"message"`
+	OST     OST_Respuesta `json:"ost"`
+}
+
+type OST_Respuesta struct {
+	Id        string                 `json:"id"`
+	Nombre    string                 `json:"nombre"`
+	Canciones []cancion.Cancion_info `json:"canciones"`
+}
 
 var _ = Describe("Routes", func() {
 	var router *gin.Engine
 	var w_v, w_p, w_s *httptest.ResponseRecorder
 	var req_v, req_p, req_s *http.Request
 	var nuevaOst_pv, nuevaOst_s gin.H
-	var body_v, body_p, body_s gin.H
+	var res_v, res_p, res_s *Respuesta
 	var canciones []gin.H
+	var err_v, err_p, err_s error
 
 	BeforeEach(func() {
 		router = server.SetupRoutes()
@@ -64,25 +77,36 @@ var _ = Describe("Routes", func() {
 
 		Context("La OST es correcta", func() {
 			BeforeEach(func() {
-				json.Unmarshal(w_v.Body.Bytes(), &body_v)
-				json.Unmarshal(w_p.Body.Bytes(), &body_p)
-				json.Unmarshal(w_s.Body.Bytes(), &body_s)
+				res_v = &Respuesta{}
+				err_v = json.Unmarshal(w_v.Body.Bytes(), res_v)
+
+				res_p = &Respuesta{}
+				err_p = json.Unmarshal(w_p.Body.Bytes(), res_p)
+
+				res_s = &Respuesta{}
+				err_s = json.Unmarshal(w_s.Body.Bytes(), res_s)
+			})
+
+			It("El JSON de respuesta no debe tener errores", func() {
+				Expect(err_v).NotTo(HaveOccurred())
+				Expect(err_p).NotTo(HaveOccurred())
+				Expect(err_s).NotTo(HaveOccurred())
 			})
 
 			It("Deberia crear la OST", func() {
-				Expect(body_v["message"]).To(Equal("OST creada"))
-				Expect(body_p["message"]).To(Equal("OST creada"))
-				Expect(body_s["message"]).To(Equal("OST creada"))
+				Expect(res_v.Message).To(Equal("OST creada"))
+				Expect(res_p.Message).To(Equal("OST creada"))
+				Expect(res_s.Message).To(Equal("OST creada"))
 
-				Expect(body_v["ost"].(map[string]interface{})["canciones"].([]interface{})[0].(map[string]interface{})["Titulo"]).To(Equal("Cancion 1"))
-				Expect(body_p["ost"].(map[string]interface{})["canciones"].([]interface{})[0].(map[string]interface{})["Titulo"]).To(Equal("Cancion 1"))
-				Expect(body_s["ost"].(map[string]interface{})["canciones"].([]interface{})[0].(map[string]interface{})["Titulo"]).To(Equal("Cancion 1"))
+				Expect(res_v.OST.Nombre).To(Equal("OST Prueba"))
+				Expect(res_p.OST.Nombre).To(Equal("OST Prueba"))
+				Expect(res_s.OST.Nombre).To(Equal("OST Prueba"))
 			})
 
 			It("La OST debería tener dos canciones", func() {
-				Expect(len(body_v["ost"].(map[string]interface{})["canciones"].([]interface{}))).To(Equal(2))
-				Expect(len(body_p["ost"].(map[string]interface{})["canciones"].([]interface{}))).To(Equal(2))
-				Expect(len(body_s["ost"].(map[string]interface{})["canciones"].([]interface{}))).To(Equal(2))
+				Expect(len(res_v.OST.Canciones)).To(Equal(2))
+				Expect(len(res_p.OST.Canciones)).To(Equal(2))
+				Expect(len(res_s.OST.Canciones)).To(Equal(2))
 			})
 
 			It("El código HTTP debe ser 200", func() {
