09472d78a3e1b1e3f16b404ed317fe5a36568066
Dunspa <jlgallego99@correo.ugr.es>
2021-12-11 18:23:09 +0100

    Comprobar que se crea la ost #67
    Accediendo a json anidado y comprobando campos iguales

--- internal/server/routes_test.go
+++ internal/server/routes_test.go
@@ -17,6 +17,8 @@ var _ = Describe("Routes", func() {
 	var w_v, w_p, w_s *httptest.ResponseRecorder
 	var req_v, req_p, req_s *http.Request
 	var nuevaOst_pv, nuevaOst_s gin.H
+	var body_v, body_p, body_s gin.H
+	var canciones []gin.H
 
 	BeforeEach(func() {
 		router = server.SetupRoutes()
@@ -24,7 +26,7 @@ var _ = Describe("Routes", func() {
 		w_p = httptest.NewRecorder()
 		w_s = httptest.NewRecorder()
 
-		canciones := []gin.H{{
+		canciones = []gin.H{{
 			"titulo":     "Cancion 1",
 			"compositor": "Compositor 1",
 			"genero":     "Rock",
@@ -61,10 +63,32 @@ var _ = Describe("Routes", func() {
 		})
 
 		Context("La OST es correcta", func() {
+			BeforeEach(func() {
+				json.Unmarshal(w_v.Body.Bytes(), &body_v)
+				json.Unmarshal(w_p.Body.Bytes(), &body_p)
+				json.Unmarshal(w_s.Body.Bytes(), &body_s)
+			})
+
+			It("Deberia crear la OST", func() {
+				Expect(body_v["message"]).To(Equal("OST creada"))
+				Expect(body_p["message"]).To(Equal("OST creada"))
+				Expect(body_s["message"]).To(Equal("OST creada"))
+
+				Expect(body_v["ost"].(map[string]interface{})["canciones"].([]interface{})[0].(map[string]interface{})["Titulo"]).To(Equal("Cancion 1"))
+				Expect(body_p["ost"].(map[string]interface{})["canciones"].([]interface{})[0].(map[string]interface{})["Titulo"]).To(Equal("Cancion 1"))
+				Expect(body_s["ost"].(map[string]interface{})["canciones"].([]interface{})[0].(map[string]interface{})["Titulo"]).To(Equal("Cancion 1"))
+			})
+
+			It("La OST debería tener dos canciones", func() {
+				Expect(len(body_v["ost"].(map[string]interface{})["canciones"].([]interface{}))).To(Equal(2))
+				Expect(len(body_p["ost"].(map[string]interface{})["canciones"].([]interface{}))).To(Equal(2))
+				Expect(len(body_s["ost"].(map[string]interface{})["canciones"].([]interface{}))).To(Equal(2))
+			})
+
 			It("El código HTTP debe ser 200", func() {
 				Expect(w_v.Code).To(Equal(200))
 				Expect(w_p.Code).To(Equal(200))
-				Expect(w_s.Code).To(Equal(201))
+				Expect(w_s.Code).To(Equal(200))
 			})
 		})
 	})
