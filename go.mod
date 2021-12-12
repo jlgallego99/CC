c80c2fb87c2b96f1a117a485ce2731d31029e7e4
Dunspa <jlgallego99@correo.ugr.es>
2021-12-12 20:57:33 +0100

    Con dotenv guardar el endpoint de etcd closes #69
    Para no hard codear el endpoint y usarlo en el futuro

--- go.mod
+++ go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/go-task/slim-sprig v0.0.0-20210107165309-348f09dbbbc0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,9 +1,11 @@
 package config
 
 import (
+	"os"
 	"time"
 
 	"github.com/jlgallego99/OSTfind/internal/server"
+	"github.com/joho/godotenv"
 	clientv3 "go.etcd.io/etcd/client/v3"
 )
 
@@ -11,8 +13,13 @@ type Config struct {
 }
 
 func (cfg *Config) ETCD() (*clientv3.Client, error) {
+	err := godotenv.Load()
+	if err != nil {
+		return nil, err
+	}
+
 	cli, err := clientv3.New(clientv3.Config{
-		Endpoints:   []string{"http://localhost:2379"},
+		Endpoints:   []string{os.Getenv("ETCD_ENDPOINT")},
 		DialTimeout: 5 * time.Second,
 	})
 
