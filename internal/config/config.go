88379d47c4841bf19079dc8849cc41cf1420134f
Dunspa <jlgallego99@correo.ugr.es>
2021-12-12 20:55:09 +0100

    Funcion para iniciar el cliente etcd #69
    Devolviendo el cliente y un codigo de error por si no se ha podido crear

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,8 +1,22 @@
 package config
 
-import "github.com/jlgallego99/OSTfind/internal/server"
+import (
+	"time"
+
+	"github.com/jlgallego99/OSTfind/internal/server"
+	clientv3 "go.etcd.io/etcd/client/v3"
+)
 
 type Config struct {
+}
+
+func (cfg *Config) ETCD() (*clientv3.Client, error) {
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{"http://localhost:2379"},
+		DialTimeout: 5 * time.Second,
+	})
+
+	return cli, err
 }
 
 func (cfg *Config) HTTP() (*server.Config, error) {
