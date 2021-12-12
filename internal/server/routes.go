73e238dba8ba8c2c25241c3661e75ccc98ace316
Dunspa <jlgallego99@correo.ugr.es>
2021-12-12 19:56:37 +0100

    Logger por defecto en salida estandar closes #71
    Indica fecha, hora, petición realizada, mensaje de error y ruta en la que se ha hecho

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,11 +1,16 @@
 package server
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRoutes() *gin.Engine {
 	router := gin.New()
+
+	router.Use(gin.Logger())
+	gin.DefaultWriter = os.Stdout
 
 	router.POST("/osts/:obra", newOST)
 
